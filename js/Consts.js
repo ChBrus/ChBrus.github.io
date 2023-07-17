c8723ff382c2d6a9b0fe5824276b6dec6057dde3
ChBrus <cbruno.carrillo@gmail.com>
2023-07-17 16:26:26 -0600

    I added an img and setting some constants

---
+++ js/Consts.js
@@ -0,0 +1,10 @@
+const ahorcado = document.querySelector(".ahorcado"),
+      wordguess = document.querySelector(".wordguess"),
+      validator = document.querySelector(".validator");
+
+const wordKey = [
+    {
+        name: "Test",
+        images: ["https://cdn-icons-png.flaticon.com/512/2916/2916315.png"]
+    }
+]
